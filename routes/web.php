3118496d166e5326d25f14e96f55c990c4ccca4c
peer <peerforcher@foxmail.com>
2018-09-17 19:39:24 +0000

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
+
+//需要把这个路由放在 auth 这个中间件的路由组里面，因为只有已经登录的用户才能看到这个提示界面。
+Route::group(['middleware' => 'auth'],function(){
+	Route::get('/email_verify_notice','PagesController@emailVerifyNotice')->name('email_verify_notice');
+	Route::group(['middleware' => 'email_verified'],function(){
+		Route::get('/test',function(){
+			return "Your email is verified";
+		});
+	});
+});
