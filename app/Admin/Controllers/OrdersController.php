087dbdc62dcfdd38f4d0969b2e9dbb573c40eb6f
peer <peerforcher@foxmail.com>
2018-09-22 18:51:58 +0000

    管理后台

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -28,6 +28,50 @@ class OrdersController extends Controller
     }
 
     /**
+     * Show interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show(Content $content,Order $order)
+    {
+        return $content
+            ->header('查看订单')
+            ->description('description')
+            ->body(view('admin.orders.show',['order' => $order]));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header('Edit')
+            ->description('description')
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header('Create')
+            ->description('description')
+            ->body($this->form());
+    }
+
+    /**
      * Make a grid builder.
      *
      * @return Grid
@@ -62,5 +106,65 @@ class OrdersController extends Controller
         });
 
         return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed   $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Order::findOrFail($id));
+
+        $show->id('Id');
+        $show->no('No');
+        $show->user_id('User id');
+        $show->address('Address');
+        $show->total_amount('Total amount');
+        $show->remark('Remark');
+        $show->paid_at('Paid at');
+        $show->payment_method('Payment method');
+        $show->payment_no('Payment no');
+        $show->refund_status('Refund status');
+        $show->refund_no('Refund no');
+        $show->closed('Closed');
+        $show->reviewed('Reviewed');
+        $show->ship_status('Ship status');
+        $show->ship_data('Ship data');
+        $show->extra('Extra');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Order);
+
+        $form->text('no', 'No');
+        $form->number('user_id', 'User id');
+        $form->textarea('address', 'Address');
+        $form->decimal('total_amount', 'Total amount');
+        $form->textarea('remark', 'Remark');
+        $form->datetime('paid_at', 'Paid at')->default(date('Y-m-d H:i:s'));
+        $form->text('payment_method', 'Payment method');
+        $form->text('payment_no', 'Payment no');
+        $form->text('refund_status', 'Refund status')->default('pending');
+        $form->text('refund_no', 'Refund no');
+        $form->switch('closed', 'Closed');
+        $form->switch('reviewed', 'Reviewed');
+        $form->text('ship_status', 'Ship status')->default('pending');
+        $form->textarea('ship_data', 'Ship data');
+        $form->textarea('extra', 'Extra');
+
+        return $form;
     }
 }
