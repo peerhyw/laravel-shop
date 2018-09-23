4c59a651d972189740fbd2f8b1e6c73055d47982
peer <peerforcher@foxmail.com>
2018-09-23 09:38:48 +0000

    用户界面-申请退款

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 use App\Services\OrderService;
 use App\Http\Requests\SendReviewRequest;
 use App\Events\OrderReviewd;
+use App\Http\Requests\ApplyRefundRequest;
 
 
 class OrdersController extends Controller
@@ -93,5 +94,27 @@ class OrdersController extends Controller
         });
 
         return redirect()->back();
+    }
+
+    public function applyRefund(Order $order,ApplyRefundRequest $request){
+        $this->authorize('own',$order);
+
+        if(!$order->paid_at){
+            throw new InvalidRequestException('该订单未支付，不可退款');
+        }
+        //判断订单退款状态是否正确
+        if($order->refund_status !== Order::REFUND_STATUS_PENDING){
+            throw new InvalidRequestException('该订单已申请过退款，请勿重复申请');
+        }
+        //将用户输入的退款理由放到订单的extra字段中
+        $extra = $order->extra ?: [];
+        $extra['refund_reason'] = $request->input('reason');
+        //将订单退款状态改为已退款
+        $order->update([
+            'refund_status' => Order::REFUND_STATUS_APPLIED,
+            'extra' => $extra,
+        ]);
+
+        return $order;
     }
 }
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -83,6 +83,17 @@
 								</div>
 							</div>
 						@endif
+						<!-- 订单已支付，且退款状态不是未退款时展示退款信息 -->
+						@if($order->paid_at && $order->refund_status !== \App\Models\Order::REFUND_STATUS_PENDING)
+							<div class="line">
+								<div class="line-label">退款状态：</div>
+								<div class="line-value">{{ \App\Models\Order::$refundStatusMap[$order->refund_status] }}</div>
+							</div>
+							<div class="line">
+								<div class="line-label">退款理由：</div>
+								<div class="line-value">{{ $order->extra['refund_reason'] }}</div>
+							</div>
+						@endif
 					</div>
 					<div class="order-summary text-right">
 						<div class="total-amount">
@@ -117,11 +128,18 @@
 							</div>
 						@endif
 						<!-- 支付按钮结束 -->
+						<!-- 如果订单的发货状态为已发货则展示确认收货按钮 -->
 						@if($order->ship_status === \App\Models\Order::SHIP_STATUS_DELIVERED)
 							<div class="receive-button">
 								<button class="btn btn-sm btn-success" type="button" id="btn-receive">
 									确认收货
 								</button>
+							</div>
+						@endif
+						<!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
+						@if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
+							<div class="refund-button">
+								<button class="btn btn-sm btn-danger" id="btn-apply-refund">申请退款</button>
 							</div>
 						@endif
 					</div>
@@ -168,6 +186,24 @@
 					.then(function(){
 						//刷新页面
 						location.reload();
+					});
+			});
+		});
+		$('#btn-apply-refund').click(function(){
+			swal({
+				text: '请输入退款理由',
+				content: "input", 
+			}).then(function(input){
+				//当用户点击swal弹出框上的按钮时触发这个函数
+				if(!input){
+					swal('退款理由不能为空','','error');
+					return;
+				}
+				//请求退款接口
+				axios.post('{{ route('orders.apply_refund',['order' => $order->id]) }}',{reason: input}).then(function(){
+						swal('申请退款成功','','success').then(function(){
+							location.reload();
+						});
 					});
 			});
 		});
